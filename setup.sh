67f034544b9b2013ecfe46fa0ed864e2b0b2c5b2
Gabriel Martin <g.martin@htlkrems.at>
2023-10-03 19:49:26 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,3 @@
+sudo apt update
+sudo apt install build-essential bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo wget nasm mtools python3 python3-pip python3-parted scons dosfstools libguestfs-tools
+
