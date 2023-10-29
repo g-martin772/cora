096e4ed9fd21c9e78d666d0f83abca800e0988d6
Gabriel Martin <g.martin@htlkrems.at>
2023-10-29 17:05:44 +0100

    GDT and 32bit PM

--- src/bootloader/bootloader.asm
+++ src/bootloader/bootloader.asm
@@ -7,18 +7,20 @@ BITS 16
 
 
 start:
-	mov si, string_to_print
-	call print_string
-
-	mov cx, 0x3ac7
-	call print_hex
-
+	call enter_protected
 	jmp $
 
-	string_to_print db "Some Value: ",0
+bits 32
+pm_start:
+	; Print a character to video memory
+	mov ah, 0x0F
+	mov al, 'h'
+	mov [0xb8000], ax
+	mov al, 'i'
+	mov [0xb8002], ax
+	jmp $
 
-
-
+bits 16
 ; BIOS Functions
 
 ; si = string address to print
@@ -85,6 +87,78 @@ print_byte:
 	mov ah, 0xe
 	int 0x10
 	ret
+
+; GDT - Global Descriptor Table
+gdt_start:
+gdt_null:	; Entry 1: Null entry must be included first (error check)
+	dd 0x0	; double word = 4 bytes = 32 bits
+	dd 0x0
+gdt_code:	; Entry 2: Code segment descriptor
+	; Structure:
+	; Segment Base Address (base) = 0x0
+	; Segment Limit (limit) = 0xfffff
+	dw 0xffff	; Limit bits 0-15
+	dw 0x0000	; Base bits 0-15
+	db 0x00		; Base bits 16-23
+	; Flag Set 1:
+		; Segment Present: 0b1
+		; Descriptor Privilege level: 0x00 (ring 0)
+		; Descriptor Type: 0b1 (code/data)
+	; Flag Set 2: Type Field
+		; Code: 0b1 (this is a code segment)
+		; Conforming: 0b0 (Code w/ lower privilege may not call this)
+		; Readable: 0b1 (Readable or execute only? Readable means we can read code constants)
+		; Accessed: 0b0 (Used for debugging and virtual memory. CPU sets bit when accessing segment)
+	db 10011010b	; Flag set 1 and 2
+	; Flag Set 3
+		; Granularity: 0b1 (Set to 1 multiplies limit by 4K. Shift 0xfffff 3 bytes left, allowing to span full 32G of memory)
+		; 32-bit default: 0b1
+		; 64-bit segment: 0b0
+		; AVL: 0b0
+	db 11001111b	; Flag set 3 and limit bits 16-19
+	db 0x00		; Base bits 24-31
+gdt_data:
+	; Same except for code flag:
+		; Code: 0b0
+	dw 0xfffff	; Limit bits 0-15
+	dw 0x0000	; Base bits 0-15
+	db 0x00		; Base bits 16-23
+	db 10010010b	; Flag set 1 and 2
+	db 11001111b	; 2nd flags and limit bits 16-19
+	db 0x00		; Base bits 24-31
+
+gdt_end:		; Needed to calculate GDT size for inclusion in GDT descriptor
+
+; GDT Descriptor
+gdt_descriptor:
+	dw gdt_end - gdt_start - 1	; Size of GDT, always less one
+	dd gdt_start
+
+; Define constants
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+
+; 32bit protected mode
+
+enter_protected:
+	; Clear screen by switching to text mode (while we still have BIOS)
+	mov ah, 0x0
+	mov al, 0x3
+	int 0x10
+
+	; Disable interrupts
+	cli
+	; Load GDT
+	lgdt [gdt_descriptor]
+
+	; Switch to PM by setting control register cr0 (use eax b/c cannot set cr0 directly)
+	mov eax, cr0
+	or eax, 0x1
+	mov cr0, eax
+
+	; Perform a far jump to flush CPU pipeline
+	jmp CODE_SEG:pm_start
 
 
 ; Bootloader signature
